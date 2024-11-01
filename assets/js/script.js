4fa4eac1edc921c9130daae091dbb2b1be599798
Nhat Tan <nhoxcon029@gmail.com>
2024-11-01 11:03:43 +0700

    update css

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   $("#copyButton").click(function () {
     const copyInput = $("#copyInput");
     copyInput.select();
-    document.execCommand("copy"); // Copies the text
+    document.execCommand("copy"); 
     $("#copyButton").text("Copied");
   });
 
@@ -16,21 +16,18 @@ $(document).ready(function () {
 
   const canvas = document.getElementById("myCanvas");
   const ctx = canvas.getContext("2d");
-
-  // Load the image
   const img = new Image();
-  img.crossOrigin = "anonymous"; // This is important for CORS
+  img.crossOrigin = "anonymous"; 
   img.src =
-    "https://upload.wikimedia.org/wikipedia/commons/d/d0/QR_code_for_mobile_English_Wikipedia.svg"; // Example image URL (replace with your image URL)
+    "https://upload.wikimedia.org/wikipedia/commons/d/d0/QR_code_for_mobile_English_Wikipedia.svg";
   img.onload = function () {
-    // Draw the image on the canvas
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
   };
 
   $("#download-qr").click(function () {
     const link = document.createElement("a");
-    link.href = canvas.toDataURL("image/png"); // Specify the image format
-    link.download = "canvas-image.png"; // Set the name for the downloaded file
+    link.href = canvas.toDataURL("image/png"); 
+    link.download = "qr.png"; 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
